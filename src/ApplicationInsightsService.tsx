7f1f428a1c12936db9608ebb508ee4b49ca85b61
Mason Torres <60087670+MasonTorres@users.noreply.github.com>
2023-09-25 13:16:21 -0700

    AppInsightContext update

---
+++ src/ApplicationInsightsService.tsx
@@ -0,0 +1,29 @@
+import {
+  ApplicationInsights,
+  ITelemetryItem,
+} from "@microsoft/applicationinsights-web";
+import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
+
+const reactPlugin = new ReactPlugin();
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString:
+      "InstrumentationKey=bdd6ab31-c0bd-4530-a120-e434c0b7a26a;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/",
+    extensions: [reactPlugin],
+    extensionConfig: {},
+    enableAutoRouteTracking: true,
+    disableAjaxTracking: false,
+    autoTrackPageVisitTime: true,
+    enableCorsCorrelation: true,
+    enableRequestHeaderTracking: true,
+    enableResponseHeaderTracking: true,
+  },
+});
+appInsights.loadAppInsights();
+
+appInsights.addTelemetryInitializer((env: ITelemetryItem) => {
+  env.tags = env.tags || [];
+  env.tags["ai.cloud.role"] = "NLL";
+});
+
+export { reactPlugin, appInsights };
