d75d4920e05063f4255fba09b5e971c1580eaff0
Mason Torres <60087670+MasonTorres@users.noreply.github.com>
2023-11-01 09:20:31 -0700

    Fix sidebar toggle

--- src/app/components/layout/NavSubMenu.tsx
+++ src/app/components/layout/NavSubMenu.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { SubMenu } from "react-pro-sidebar";
-// Iconify Icons
-import linuxIcon from "@iconify/icons-cib/linux";
 
 export default function NavSubMenu({
   level,
@@ -19,27 +17,17 @@ export default function NavSubMenu({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
-  const [open, setOpen] = useState(false);
-  const [rand, setRand] = useState(0); // [rand, setRand
-  const handleOpen = () => {
-    setOpen(!open);
+  const [open, toggleOpen] = useState(false);
+  const handleMenuToggle = () => {
+    toggleOpen(!open);
   };
-
-  useEffect(() => {
-    if (pathname.startsWith(path) && open === false) {
-      console.log("Settings open to true");
-      setOpen(true);
-      // New random number to force re-render
-      setRand(Math.random());
-    }
-  }, [pathname, open, path]);
 
   return (
     <SubMenu
-      key={path + rand}
+      key={path}
       defaultOpen={pathname.startsWith(path) ? true : undefined}
       open={open}
-      onClick={() => handleOpen()}
+      onClick={() => handleMenuToggle()}
       label={label}
     >
       {children}
